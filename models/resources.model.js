68bd94acac1ce19f7a377814aa3c09a772f99948
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-03-04 22:58:07 +0100

    im back!!!

--- models/resources.model.js
+++ models/resources.model.js
@@ -1,8 +1,32 @@
-
-// for post of the day
-id
-datePosted
-content
-comments
-
 // resources include videos and articles
+
+const { Schema, model, Types } = require("mongoose");
+
+// Declare the Schema of the Mongo model
+const resourceSchema = new Schema(
+  {
+    datePosted: {
+      type: Date,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["video", "article"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+const Resource = model("Resource", resourceSchema);
+
+module.exports = {
+  Resource,
+};
