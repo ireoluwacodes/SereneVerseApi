171fa0c0b1edc9468aa0bbbc489d938a8e8a47f2
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-05-06 22:43:52 +0100

    prod fix

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,10 +1,12 @@
 const multer = require("multer");
 const path = require("path");
+const { nodeEnv } = require("../config/constants.config");
 
 const multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // cb(null, path.join(__dirname, "../tmp"));
-    cb(null, "/tmp");
+    nodeEnv == "production"
+      ? cb(null, "/tmp")
+      : cb(null, path.join(__dirname, "../tmp"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
