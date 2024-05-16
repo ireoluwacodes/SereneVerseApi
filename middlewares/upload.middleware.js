a1c43284f421aa90ae640ad329e4fe0b979860a7
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-05-16 22:51:16 +0100

    push

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -4,9 +4,10 @@ const { nodeEnv } = require("../config/constants.config");
 
 const multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    nodeEnv == "production"
-      ? cb(null, "/tmp")
-      : cb(null, path.join(__dirname, "../tmp"));
+    // nodeEnv == "production"
+    //   ? cb(null, "/tmp")
+    //   : cb(null, path.join(__dirname, "../tmp"));
+    cb(null, "/tmp")
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
