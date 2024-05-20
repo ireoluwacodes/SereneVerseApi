1d5629cb7a6de51f7d70ecd36326bb0bcd8c1c86
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-05-20 23:02:24 +0100

    fix

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ const startApp = async (port) => {
 startApp(PORT).then(() => {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+      // origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+      origin: "*",
       methods: ["POST, GET, PUT, PATCH, DELETE"],
-      credentials: true,
+      credentials: false,
     },
   });
 
