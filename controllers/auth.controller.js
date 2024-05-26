1bed6e6f3996dc18a8820b83f112ac468addd5db
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-05-26 09:32:08 +0100

    tkn

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -282,7 +282,7 @@ const confirmOtp = AsyncHandler(async (req, res, next) => {
 const refresh = AsyncHandler(async (req, res, next) => {
   try {
     // destructure existing refresh token from the cookies sent to the browser in the log in endpoint
-    const { refresh_token } = req.cookies;
+    const refresh_token = req.cookies.refreshtkn;
 
     //fetch userId attached to request object from authMiddleware
 
